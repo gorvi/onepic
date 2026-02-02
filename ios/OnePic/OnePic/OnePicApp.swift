fc6730f7dd4c4c1debf28ac1117d5b9fcde64993
gaohongwei <gorvi@163.com>
2026-02-02 12:47:44 +0800

    Refactor project structure: Move Android project to android/ and initialize ios/ directory

---
+++ ios/OnePic/OnePic/OnePicApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnePicApp.swift
+//  OnePic
+//
+//  Created by gaohongwei on 2026/2/2.
+//
+
+import SwiftUI
+
+@main
+struct OnePicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
